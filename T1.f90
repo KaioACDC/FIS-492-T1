4eaf202936860b0905caff2aa2b6eb4d797b30cb
Kaio <kaio.chagas@ufv.br>
2024-11-14 19:28:35 -0300

    Calculando desvio padrão

--- T1.f90
+++ T1.f90
@@ -22,3 +22,10 @@ contains
         vvariancia = sum((valor - y)**2) / real(x-1)
     end function variancia
 
+!Vamos calcular o desvio padrão
+    function desviop(vvariancia) result(vdesvio)
+        !Definindo dimensão dos valores
+        real, intent(in) :: vvariancia
+        real :: vdesvio
+        vdesvio = sqrt(vvariancia)
+    end function desviop
