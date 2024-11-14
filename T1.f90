358fb713994df61da8ad58e6fae31abd7cb70b35
Kaio <kaio.chagas@ufv.br>
2024-11-14 19:25:17 -0300

    Calculando variância

--- T1.f90
+++ T1.f90
@@ -5,11 +5,20 @@ contains
 
 !Vamos calcular a média
     function media(valor, x) result(y)
-        !Definindo 
+        !Definindo dimensão dos valores
         real, dimension(:), intent(in) :: valor
         integer, intent(in) :: x
         real :: y
-        y = sum(valor)/ real(x)
+        y = sum(valor) / real(x)
     end function media
 
-    
+!Vamos calcular a variância
+    function variancia(valor, x, y) result(vvariancia)
+        !Definindo dimensão dos valores
+        real, dimension(:), intent(in) :: valor
+        integer, intent(in) :: x
+        real, intent(in) :: y
+        real :: vvariancia
+        vvariancia = sum((valor - y)**2) / real(x-1)
+    end function variancia
+
